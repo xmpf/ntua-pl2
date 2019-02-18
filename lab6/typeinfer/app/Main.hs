147367bc768dddc00887de7993318aa5ff1bce42
Michalis Papadopoullos <mechatr0nic.cy@gmail.com>
2019-02-18 17:05:21 +0200

    minor changes on typeinfer solution

--- lab6/typeinfer/app/Main.hs
+++ lab6/typeinfer/app/Main.hs
@@ -4,13 +4,19 @@ import Text.Read      -- read
 import Data.Maybe     -- maybe
 
 -- datatypes
-data Type   = Tvar Int | Tfun Type Type                        deriving Eq
-data Expr   = Evar String | Eabs String Expr | Eapp Expr Expr  deriving Eq
+data Type = Tvar Int 
+          | Tfun Type Type
+          deriving Eq
+
+data Expr = Evar String
+          | Eabs String Expr
+          | Eapp Expr Expr
+          deriving Eq
 
 -- type synonyms
-type Constr = [(Type, Type)]
-type Rules  = [(Type, Type)]
-type Env    = [(String, Int)]
+type Constraint = (Type, Type)
+type Constraints = [Constraint]
+type Env = [(String, Int)]
 
 -- Pretty printing of expressions
 always = True    -- False omits parentheses whenever possible
@@ -46,12 +52,13 @@ instance Show Type where
     showParen (p > 0) (showsPrec 1 sigma . (" -> " ++) . showsPrec 0 tau)
 
 -- Populate Constraints
-mkConstraints :: Expr -> Env -> Int -> Maybe (Type, Constr, Int)
+mkConstraints :: Expr -> Env -> Int -> Maybe (Type, Constraints, Int)
 mkConstraints (Evar str) env var = do
     lookup str env >>= \t -> Just (Tvar t, [], var)
 mkConstraints (Eabs str expr) env var = do
-    mkConstraints expr ((str, var + 1) : env) (var + 1) >>=
-      \(a, b, c) -> Just ((Tfun (Tvar (var + 1)) a), b, c + 1)
+    mkConstraints expr ((str, var') : env) var' >>=
+      \(a, b, c) -> Just ((Tfun (Tvar var') a), b, 1 + c)
+      where var' = 1 + var
 mkConstraints (Eapp expr1 expr2) env var = do
     mkConstraints expr1 env var >>=
       \(a, b, c) -> mkConstraints expr2 env c >>=
@@ -63,44 +70,43 @@ tLookup t1 t2@(Tvar a) = t1 == t2
 tLookup t1 t2@(Tfun t' t'') = (tLookup t1 t') || (tLookup t1 t'')
 
 -- replace a type in type
-tReplace :: Type -> Type -> Constr -> Constr -> Constr
+tReplace :: Type -> Type -> Constraints -> Constraints -> Constraints
 tReplace _ _ [] c' = c'
 tReplace ta tb ((ta', tb') : c) c' = tReplace ta tb c ((ta'', tb'') : c')
   where ta'' = replace ta tb ta'
         tb'' = replace ta tb tb'
-        replace a b c@(Tvar d)
-          | a == c = b
-          | otherwise = c
+        replace a b c@(Tvar d) = case (a == c) of
+                                  True -> b
+                                  False -> c
         replace a b c@(Tfun e' e'') = Tfun (replace a b e') (replace a b e'')
 
 -- unify W algorithm
-unify :: Constr -> Rules -> Maybe Rules
+unify :: Constraints -> Constraints -> Maybe Constraints
 unify [] r = Just r
-unify ((ta, tb) : tc) r
-  | (ta == tb) = unify tc r
-unify ((ta@(Tvar a), tb) : tc) r
-  | not (tLookup ta tb) = unify (tReplace ta tb tc []) ((ta, tb) : r)
-unify ((ta, tb@(Tvar b)) : tc) r 
-  | not (tLookup tb ta) = unify (tReplace tb ta tc []) ((tb, ta) : r)
+unify ((ta, tb) : tc) r | (ta == tb) = unify tc r
+unify ((ta@(Tvar a), tb) : tc) r | not (tLookup ta tb) = unify (tReplace ta tb tc []) ((ta, tb) : r)
+unify ((ta, tb@(Tvar b)) : tc) r | not (tLookup tb ta) = unify (tReplace tb ta tc []) ((tb, ta) : r)
 unify ((ta@(Tfun ta' ta''), tb@(Tfun tb' tb'')) : tc) r = unify ((ta', tb') : (ta'', tb'') : tc) r
 unify _ _ = Nothing
 
-sub1 :: Type -> Rules -> Type
+-- substitute
+sub1 :: Type -> Constraints -> Type
+sub1 t@(Tfun ta tb) r = Tfun (sub1 ta r) (sub1 tb r)
 sub1 ta@(Tvar a) r =
     case lookup ta r of
         Just t' -> replace ta t' ta
         Nothing -> ta
-    where
-    replace a b c@(Tvar d)
-      | a == c = b
-      | otherwise = c
-    replace a b c@(Tfun e' e'') = Tfun (replace a b e') (replace a b e'')
-sub1 t@(Tfun ta tb) r = Tfun (sub1 ta r) (sub1 tb r)
+    where replace :: Type -> Type -> Type -> Type
+          replace a b c@(Tvar d) = case (a == c) of
+                                      True -> b
+                                      False -> c
+          replace a b c@(Tfun e' e'') = Tfun (replace a b e') (replace a b e'')
 
-sub' :: Type -> Rules -> Maybe Type
+sub' :: Type -> Constraints -> Maybe Type
 sub' t r = do
     x <- pure $ sub1 t r 
-    if t == x then Just t else sub' x r
+    if t == x then Just t 
+    else sub' x r
 
 
 -- enumerate types 'lexicographically'
@@ -117,14 +123,17 @@ enumTypes t = Just (sub1 t (fst sorted_types))
             in (subs2, counter2)
 
 -- Main program
-typeInfer = do  s <- getLine
-                let e = read s :: Expr
-                let type' = mkConstraints e [] 0 >>=
-                          \(t, c, _) -> unify c [] >>=
-                          sub' t >>= enumTypes
-                maybe (putStrLn "type error") print type'
+typeInfer = do
+    s <- getLine
+    let e = read s :: Expr
+    let type' = mkConstraints e [] 0 >>=
+              \(t, c, _) -> unify c [] >>=
+              sub' t >>= enumTypes
+    maybe (putStrLn "type error") print type'
 
 count n m  =  sequence $ take n $ repeat m
 
-main     =  do  n <- readLn
-                count n typeInfer
+main :: IO ()
+main = do
+  n <- readLn
+  count n typeInfer >> return ()
